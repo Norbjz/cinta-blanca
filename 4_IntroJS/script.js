de04da8c917f706ae063adc2a9ec095f43d7d4b7
Norberto Jiménez <norberto.j90@gmail.com>
2020-08-21 21:33:39 -0500

    Intro JS

---
+++ 4_IntroJS/script.js
@@ -0,0 +1,50 @@
+var saludo = 'Hola desde saludo'; //string
+
+var edad = 18; // Valor numérico
+
+var verdadero = true; //Booleanos
+var falso = false; //Booleanos
+
+var arreglo = []; //Areglos
+
+var frutas = ['manzana', 'naranja', 'pera']
+// frutas[2] -- naranja
+
+var objeto = {} //Objetos con propiedades
+
+var yo = {
+    nombre: 'Norberto',
+    edad: 30,
+}
+
+// yo.nombre -- Norberto
+
+//Output
+console.log('Hola');
+console.log(frutas);
+alert('¡Código Rojo!');
+
+//Input
+prompt('Ingresa un numero');
+
+var nom = prompt('Ingresa tu nombre', 'Juan Perez') //Argumentos
+var email = prompt('Ingresa tu email', 'ejemplo@gmail.com') 
+console.log(nom);
+
+//Sumar
+var num1 = 100;
+var num2 = 200;
+console.log(num1 + num2);
+
+//Concatenar
+var nombre = 'Ramirito'
+console.log('Hola, soy ' + nombre);
+
+var numX = 100
+
+console.log(numX);
+
+var numX = 200
+var numx = 50
+
+console.log(numX, numx);
