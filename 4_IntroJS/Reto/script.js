f4b34848cfe9646c7d4b5ff87b27fff6dee1f5ba
Norberto Jiménez <norberto.j90@gmail.com>
2020-08-21 22:16:39 -0500

    Reto JS

---
+++ 4_IntroJS/Reto/script.js
@@ -0,0 +1,8 @@
+var perro = 'Pedrito'
+var gato = 'Rufo'
+var pollo = 'Chancla'
+console.log('Mis mascotas son ' + perro + ', ' + gato + ' y ' + pollo + '. Les quiero mucho.');
+
+var mascota1 = prompt('Ingresa el nombre de tu mascota')
+var mascota2 = prompt('Ingresa el nombre de tu otra mascota')
+console.log('Quiero mucho a mis mascotas: ' + mascota1 + ' y ' + mascota2 + '.');
