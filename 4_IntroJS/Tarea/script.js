9df17adef617348c2c3b8f788f03ae60cf1628fa
Norberto Jiménez <norberto.j90@gmail.com>
2020-08-24 20:06:58 -0500

    Agregado nuevo método

--- 4_IntroJS/Tarea/script.js
+++ 4_IntroJS/Tarea/script.js
@@ -7,3 +7,15 @@ var num = + text;
 
 console.log(num);
 console.log(typeof num);
+
+// Esta versión es diferente:
+/* 
+var text2 = '200';
+
+console.log(text2);
+console.log(typeof text2);
+
+var num2 = parseInt(text2)
+
+console.log(num2);
+console.log(typeof num2); */
