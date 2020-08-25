9906674e48b362e155fe27d4085b5dee92c53a65
Norberto Jiménez <norberto.j90@gmail.com>
2020-08-24 21:50:23 -0500

    Reto 2

--- 5_Condicionales/Ejercicios/main.js
+++ 5_Condicionales/Ejercicios/main.js
@@ -6,12 +6,24 @@ console.log('Hola desde retos! Todo bien!');
 // Si la contraseña es correcta, mostrar en consola: 'Bienvenido'
 // Si la constraseña no es correcta, mostrar en consola: 'Password incorrecto, intente de nuevo'
 
-var pass = 'pollo';
+/* var pass = 'pollo';
 
-var contraseña = prompt('Ingresa tu contraseña');
+var contraseña = prompt('Ingresa tu contraseña:', '*********');
 
 if (pass === contraseña){
     alert('Bienvenido.');
   } else {
     alert('Password incorrecto, intente de nuevo.');
   }
+ */
+// ------- RETO 2: PAR O IMPAR
+// Le van a preguntar al usuario que les dé un número.
+// Van a decir si este numero es par o es impar.
+
+var num = prompt('Ingresa un número:');
+
+if (num % 2 === 0) {
+    alert ('Tu numero es par')
+} else {
+    alert ('Tu número es impar')
+}
