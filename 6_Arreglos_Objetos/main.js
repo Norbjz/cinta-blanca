fc6f5e5cf2ee9e754b6f8d9cdb709841811da432
Norberto Jiménez <norberto.j90@gmail.com>
2020-08-25 22:05:49 -0500

    Objetos y Json

--- 6_Arreglos_Objetos/main.js
+++ 6_Arreglos_Objetos/main.js
@@ -60,3 +60,62 @@ musica.splice(2,1, 'PerritoSalchicha');
 musica.pop();
 console.log(musica);
 
+//Dato curioso: Las cadenas también se pueden interpretar como arreglos de caracteres
+var arregloCadena = 'Norberto Jiménez';
+console.log(arregloCadena[0], arregloCadena[1], arregloCadena[2]); // Nor
+
+// Cómo coloco los elementos del arreglo al revés
+// arreglo.reverse()
+
+colores.reverse()
+console.log(colores);
+
+
+// Referencia: https://www.w3schools.com/js/js_array_methods.asp
+
+//OBJETOS
+//Se delcaran objetos utilizando llaves
+//Cada elemento que contiene se considera una propiedad o llave/key
+//objeto = { key: valor }
+
+var objeto = {};
+
+var persona = { 
+    nombre: 'Norberto', 
+    apellido: 'Jimenez',
+    edad: 30,
+    mail: 'hola@gmail.com',
+    musica: {
+        jmusic: ['AKFG', 'Indigo la End', 'One ok Rock'],
+        metal: ['In Flames', 'Disarmonia Mundi', 'Trivium']
+    },
+}
+
+console.log(persona);
+
+//Para acceder a las propiedades del objeto utilizamos la siguiente estructura:
+
+console.log(persona.nombre);
+console.log(persona.musica.metal[1]);
+
+//FORMATO JSON
+//Formato con que intercambiamos info en la red de manera mas simple a diferencia del formato XML
+//Este formato si requiere comilla doble
+
+var personaJson = {
+    "nombre": "Norberto",
+    "ciudad": "Tlaxcala",
+    "edad": 30,
+    "musica": {
+        "reggeaton": {
+        "jbalvin": ["Rojo", "Blanco", "Negro"],
+        "badbunny": ["Caro", "La Santa"]
+        },
+        "cumbia": "Los Ángeles Azules",
+        "rock": "AC/DC"
+    },
+    "peliculas": ["Terror", "Documental", "Comedia"]
+}
+
+console.log(personaJson);
+
