4e43c068daa2d6571171509f3a6cacdbf2e63624
Norberto Jiménez <norberto.j90@gmail.com>
2020-08-26 20:24:24 -0500

    Retos 1

--- 7_Ciclos_For/main.js
+++ 7_Ciclos_For/main.js
@@ -1,4 +1,4 @@
-console.log('Hola desde consola.');
+/* console.log('Hola desde consola.');
 
 //CICLOS FOR
 //Repite bloques de codigo donde cada repetición se considera una iteración
@@ -15,5 +15,25 @@ for (var i = 0; i <= 100; i++) {
 //i = i + 2 se puede abreviar i +=2
 
 for (var i = 0; i <= 100; i += 2) {
+    console.log(i);
+}
+ */
+
+//Reto 1
+//Empezar en 10, termine en -250 y tiene que avanzar de 1 en 1
+
+for (var i = 10; i >= -250; i--) {
+    console.log(i);
+}
+
+//Empezar en -50, llegar a 500 y avanzar de 5 en 5
+
+for (var i = -50; i <= 500; i += 5) {
+    console.log(i);
+}
+
+//Empezar en 1000, llegar a 2000 y avanzar de 10 en 10
+
+for (var i = 1000; i <= 2000; i += 10) {
     console.log(i);
 }
