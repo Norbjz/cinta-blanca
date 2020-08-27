53eb7f7a26bce015e659aca476d5dcd12c7991e2
Norberto Jiménez <norberto.j90@gmail.com>
2020-08-26 22:13:08 -0500

    Agregados ejercicios reto 2

--- 7_Ciclos_For/main.js
+++ 7_Ciclos_For/main.js
@@ -19,6 +19,7 @@ for (var i = 0; i <= 100; i += 2) {
 }
  */
 
+/*  
 //Reto 1
 //Empezar en 10, termine en -250 y tiene que avanzar de 1 en 1
 
@@ -36,4 +37,62 @@ for (var i = -50; i <= 500; i += 5) {
 
 for (var i = 1000; i <= 2000; i += 10) {
     console.log(i);
+}
+ */
+
+
+//Ligar ciclos con arreglos
+//Uso más común para ciclar arreglos
+
+/* var alumnos = [
+    'Ricardo',
+    'Carlos',
+    'Beth',
+    'Veronica',
+    'Dalia',
+    'Justin'
+]
+
+console.log(alumnos);
+console.log(alumnos.length);
+
+alumnos.push ('Mateo')
+
+console.log(alumnos);
+console.log(alumnos.length);
+
+for (let index = 0; index < alumnos.length; index++) {
+    console.log(index+1 + '.-' + alumnos[index]);    
+} */
+
+//Reto 2
+
+var platillos = ['Enchiladas','Taco', 'Tamal', 'Pozole', 'Barbacoa', 'Guacamole' ]
+
+console.log('Ejercicio 1');
+for (var i = 0; i < platillos.length; i++) {
+    console.log(i+1 + '.- ' + platillos[i]);
+}
+
+console.log('***********');
+console.log('Ejercicio 2');
+
+for (var i = 0; i < platillos.length; i++) {
+    if (platillos[i] == 'Pozole' | platillos[i] == 'Barbacoa') {
+        console.log(platillos[i]);
+    }
+}
+
+console.log('***********');
+console.log('Ejercicio 3');
+for (var i = 0; i < platillos.length; i++) {
+    if ( i % 2 === 0) {
+    console.log(platillos[i]);
+    }
+}
+
+console.log('***********');
+console.log('Ejercicio 4');
+for (var i = 1; i < platillos.length; i +=2) {
+    console.log(platillos[i]);
 }
