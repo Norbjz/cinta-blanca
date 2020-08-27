e14b085d38d2008e1e52524ecc1149183c36f526
Norberto Jiménez <norberto.j90@gmail.com>
2020-08-26 20:08:22 -0500

    Clase Ciclos For

---
+++ 7_Ciclos_For/main.js
@@ -0,0 +1,19 @@
+console.log('Hola desde consola.');
+
+//CICLOS FOR
+//Repite bloques de codigo donde cada repetición se considera una iteración
+//Estructura
+
+//for (inicio; condicion; secuencia) {
+//  tarea a repetir
+//}
+
+for (var i = 0; i <= 100; i++) {
+    console.log(i);
+}
+
+//i = i + 2 se puede abreviar i +=2
+
+for (var i = 0; i <= 100; i += 2) {
+    console.log(i);
+}
