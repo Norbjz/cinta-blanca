8d2a534dc792999d273088ef8dc0eb9189f7ba3f
Norberto Jiménez <norberto.j90@gmail.com>
2020-08-28 18:55:19 -0500

    Tarea FizzBuzz

---
+++ 7_Ciclos_For/Tarea/main.js
@@ -0,0 +1,39 @@
+//-------> Challenge Final <-------
+// 1 -> 100
+// Si el numero es divisible entre 3 -> Fizz 
+// Si el numero es divisible entre 5 -> Buzz
+// Si el numero es divisible entre 3 y 5 -> FizzBuzz
+//---- 👍 -----
+// 0
+// 1
+// 2
+// 3 Fizz
+// 4 
+// 5 Buzz
+// ...
+// 15 Fizbuzz
+//----- 🚫 -----
+// 15 fizz
+// 15 buzz
+// 15 fizzbuzz
+
+
+for (var i = 0; i <= 100; i++) {
+    if (i === 0) {
+        console.log(i);
+    } else { 
+        if (i % 3 === 0 &&  i % 5 === 0) {
+            console.log(i + ' FizzBuzz');
+        } else {
+            if (i % 3 === 0) {
+            console.log(i + ' Fizz');
+            } else {
+                if (i % 5 === 0) {
+                console.log(i + ' Buzz');
+                } else {
+                    console.log(i);
+                }
+            }
+        }
+    }
+}   
