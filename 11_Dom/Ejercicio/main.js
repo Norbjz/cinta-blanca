98311bf0ee3d8c10d13de6e3efaa428618d41e51
Norberto Jiménez <norberto.j90@gmail.com>
2020-09-03 22:01:22 -0500

    Formato a ejercicio piña

--- 11_Dom/Ejercicio/main.js
+++ 11_Dom/Ejercicio/main.js
@@ -2,19 +2,16 @@ let nombre = document.getElementById("nombre");
 let direccion = document.getElementById("direccion");
 let telefono = document.getElementById("telefono");
 
-
 const pizza = () => {
     card1.innerHTML = nombre.value;
     card2.innerHTML = direccion.value;
     card3.innerHTML = telefono.value;
-    
-    if(document.getElementById('pinia').checked) {
+
+    if (document.getElementById("pinia").checked) {
         card4.innerHTML = "Con piña";
     } else {
         card4.innerHTML = "Sin piña";
     }
-    
 };
-
 
 boton.addEventListener("click", pizza);
